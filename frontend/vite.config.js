d8621fa02aa803d978f783d80b03d08fae55fbbd
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-11-19 21:11:50 +0530

    chore: `containerize` the fe using `docker`

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,4 +14,14 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
     },
   },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
