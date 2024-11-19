2d90c690fabea1d25b268caae31c408dbcf3f33c
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-11-19 21:59:08 +0530

    feat: backend books api service

--- frontend/src/services/books.js
+++ frontend/src/services/books.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 const booksAPI = axios.create({
   baseURL: import.meta.env.VITE_BOOKS_API_URL,
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
 });
 
 export const getBooks = async (params) => {
-  return booksAPI.get("/", { params });
+  console.log(import.meta.env.VITE_BOOKS_API_URL);
+  return booksAPI.get("", { params });
 };
